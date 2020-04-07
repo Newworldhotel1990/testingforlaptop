02ca062e20395eb844f7eaecbcd544953a5b93c3
newworldhotel1990@gmail.com <newworldhotel1990@gmail.com>
2020-04-07 12:10:22 +0800

    4th

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -4,12 +4,12 @@ const geocode = (address, callback) => {
   const url =
     "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
     address +
-    ".json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1";
+    ".json?access_token=pk.eyJ1Ijoia29rb251dGtpbmciLCJhIjoiY2s4YnVuN3YxMGN1ejNsbnM4eHZpY2N5eiJ9.bvfUfXqszoaQ2tD-DId6Qg&limit=1";
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect to location services!", undefined);
-    } else if (body.features.length == 0) {
+    } else if (body.features.length === 0) {
       callback("Unable to find location. Try another search.", undefined);
     } else {
       callback(undefined, {
